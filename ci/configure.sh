44d4cf577b29d581188897f16ea8e610a42c678e
Joseph Palermo <jpalermo@pivotal.io>
2021-08-04 23:29:06 -0700

    Move pipeline to bosh-ecosystem concourse

--- ci/configure.sh
+++ ci/configure.sh
@@ -1,4 +1,8 @@
-fly -t main-cpi sp -p bosh-cpi-ruby \
+#!/usr/bin/env bash
+
+set -eu
+
+fly -t bosh-ecosystem sp -p bosh-cpi-ruby \
   -c ci/pipeline.yml \
   -l <( lpass show --notes "bosh-cpi-ruby gem") \
   -l <( lpass show --notes pivotal-tracker-resource-keys )
