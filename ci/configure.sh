dd47b17babb3dba335ba10348d5df8f78c2506a2
Joseph Palermo <jpalermo@pivotal.io>
2023-01-17 23:17:24 -0800

    Update ci pipeline with new credhub entries

--- ci/configure.sh
+++ ci/configure.sh
@@ -3,6 +3,4 @@
 set -eu
 
 fly -t bosh-ecosystem sp -p bosh-cpi-ruby \
-  -c ci/pipeline.yml \
-  -l <( lpass show --notes "bosh-cpi-ruby gem") \
-  -l <( lpass show --notes pivotal-tracker-resource-keys )
+  -c ci/pipeline.yml
