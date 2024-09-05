982fc65f9f6f18b532b2faa616f4567afc172f6c
Joseph Palermo <jpalermo@pivotal.io>
2024-09-05 15:35:10 -0700

    Update pipeline to use a modern version of ruby

--- ci/tasks/test-unit.sh
+++ ci/tasks/test-unit.sh
@@ -2,26 +2,7 @@
 
 set -e
 
-print_git_state() {
-  if [ -d ".git" ] ; then
-    echo "--> last commit..."
-    TERM=xterm-256color git --no-pager log -1
-    echo "---"
-    echo "--> local changes (e.g., from 'fly execute')..."
-    TERM=xterm-256color git --no-pager status --verbose
-    echo "---"
-  fi
-}
-
-: ${RUBY_VERSION:="2.3.1"}
-
-source /etc/profile.d/chruby.sh
-chruby $RUBY_VERSION
-
 pushd bosh-cpi-ruby-gem
-  print_git_state
-
-  export PATH=/usr/local/ruby/bin:$PATH
-  bundle install
+  bundle
   bundle exec rspec spec
 popd
